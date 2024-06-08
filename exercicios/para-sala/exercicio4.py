f11a9965225bcce100a521362a359c78d63d995b
Heloisa <heloisa.srf@gmail.com>
2024-06-08 15:38:27 -0300

    vai2

---
+++ exercicios/para-sala/exercicio4.py
@@ -0,0 +1,7 @@
+nota1 = input('Digite a Nota 1: ')
+nota2 = input('Digite a Nota 2: ')
+nota3 = input('Digite a Nota 3: ')
+nota4 = input('Digite a Nota 4: ')
+media = (float(nota1) + float(nota2) + float(nota3) + float(nota4)) / 4
+
+print(f'A media das notas é {media}')
