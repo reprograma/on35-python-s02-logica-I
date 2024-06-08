558f662c0afa9f603218572d376f295664cb67a6
Anne Ribeiro <anne.asribeiro@gmail.com>
2024-06-08 15:52:57 -0300

    exercicio-03.py

---
+++ exercicios/para-sala/exercicio-03.py
@@ -0,0 +1,8 @@
+# 3. Faça um Programa que peça dois números e imprima a soma.
+
+numero1 = int(input("Insira o primeiro número: "))
+numero2 = int(input("Insira o segundo número: "))
+
+soma = numero1 + numero2
+
+print(f"A soma de {numero1} e {numero2} é {soma}.")
