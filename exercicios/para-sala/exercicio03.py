6f9d49bec37b10d3973a81fa24dcbbebac4842b6
Fernanda Bueno <fercrbueno24@gmail.com>
2024-06-08 16:22:33 -0300

    correcao ex 03

--- exercicios/para-sala/exercicio03.py
+++ exercicios/para-sala/exercicio03.py
@@ -2,6 +2,7 @@
 
 # Faça um Programa que peça dois números e imprima a soma.
 
-num1 = 25
-num2 = 45
-print (num1 + num2)
+num1 = input("Por favor, digite um número : ")
+num2 = input("Por favor, digite um número : ")
+soma = (num1 + num2)
+print(soma)
