78486140c6d323dced7083ff78370d9244733194
Rafaela Carvalho <carvalho.sims@gmail.com>
2024-06-08 17:09:05 -0300

    first commit

---
+++ exercicios/para-sala/exer3.py
@@ -0,0 +1,8 @@
+#3. Faça um Programa que peça dois números e imprima a soma.
+
+numero1 = input("Insira seu primeiro numero ")
+numero2 = input("Insira seu segundoo numero ")
+
+soma = (float(numero1) + float(numero2))
+
+print(f'Soma dos numeros é {soma}')
---
+++ exercicios/para-sala/exer5.py
@@ -0,0 +1,7 @@
+#5. Faça um Programa que converta metros para centímetros.
+
+metros = (input('Insira a quantidade de metros '))
+
+centimetros = float(metros) * 100
+
+print(f'Total em centímetros {int(centimetros)}cm')
