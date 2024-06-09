d32cf28b6af072fd977d793e7a4f5a408e97daa7
unknown <rafaellaroden@gmail.com>
2024-06-09 15:43:23 -0300

    incluindo testes

---
+++ exercicios/para-sala/testes_python.py
@@ -0,0 +1,12 @@
+nome_da_professora = "Agnes"
+idade_da_professora = 30
+altura_da_professora = 1.83
+mora_em_curitiba = True
+mora_em_sao_paulo = False
+
+lista_de_pets = ["Aurora", "Estrela"]
+
+lista_de_pets[1]
+print(lista_de_pets)
+
+print(lista_de_pets [0])
