bd0bfad86648487db00aec9f079e1a85ecaf108f
unknown <rafaellaroden@gmail.com>
2024-06-10 18:15:13 -0300

    atualização_testes

--- exercicios/para-sala/testes_python.py
+++ exercicios/para-sala/testes_python.py
@@ -1,12 +1,37 @@
-nome_da_professora = "Agnes"
-idade_da_professora = 30
-altura_da_professora = 1.83
-mora_em_curitiba = True
-mora_em_sao_paulo = False
+nota1 = 10
+nota2 = 6.5
+media = (nota1 + nota2) / 2
 
-lista_de_pets = ["Aurora", "Estrela"]
+saldo = 1000
+divida = 850
+resto = saldo - divida
 
-lista_de_pets[1]
-print(lista_de_pets)
+numero = 12
+resto_da_divisao = 13 % 2
+print(resto_da_divisao)
 
-print(lista_de_pets [0])
+
+numero = 15
+numero_resultado = 15 ** 2
+
+rafaella = {
+    "idade": "28",
+    "nome": "rafaella",
+    "altura": "1.69",
+    "cidade": "Joinville",
+    "trabalha_com": "RH",
+    "tem_pet": False
+}
+
+apresentacao = f"Oi, meu nome é {rafaella["nome"]}, eu tenho {rafaella["idade"]}, minha altura é {rafaella["altura"]}"
+print(apresentacao)
+
+nota1 = 10
+nota2 = 12
+nota3 = 5
+nota1 += nota2 + nota3 # atribuicao de incremento
+print (nota1)
+
+
+decaiu_ou_manteve = nota3 >= nota2
+print(decaiu_ou_manteve)
