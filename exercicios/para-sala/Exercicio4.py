10246f4052a94d180ae862a5877f6e5a8c696e92
unknown <rafaellaroden@gmail.com>
2024-06-10 22:22:53 -0300

    incluindo exercicios de sala

---
+++ exercicios/para-sala/Exercicio4.py
@@ -0,0 +1,9 @@
+nota1 = float(input("Informe a nota 1: "))
+nota2 = float(input("Informe a nota 2: "))
+nota3 = float(input("Informe a nota 3: "))
+nota4 = float(input("Informe a nota 4: "))
+
+media = (nota1+nota2+nota3+nota4) / 4
+
+print(f"A média bimestral é de {media}")
+
