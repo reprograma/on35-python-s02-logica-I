acc84150ce781564c7389b569020daa6a5098b72
unknown <rafaellaroden@gmail.com>
2024-06-11 20:49:17 -0300

    inclusão exercicio 2 - agora vai

--- exercicios/para-casa/exercicio_Rafaella2.py
+++ exercicios/para-casa/exercicio_Rafaella2.py
@@ -1,13 +1,10 @@
-Tamanho_do_arquivo_MB_inserido = input("Insira o tamanho do arquivo para download em MB: ")
-Tamanho_do_arquivo_MB = float(Tamanho_do_arquivo_MB_inserido)
-Velocidade_do_link_inserido = input("Insira a velocidade do link da internet: ")
-Velocidade_do_link = float(Velocidade_do_link_inserido)
-Tamanho_arquivo_segundos = Velocidade_do_link * 8
-print(Tamanho_arquivo_segundos)
-Segundos_para_Minutos = 60
-Resultado_em_minutos = (Tamanho_arquivo_segundos / Segundos_para_Minutos)
-print(Resultado_em_minutos)
+Tamanho_do_arquivo_MB_inserido = float(input( "Insira o tamanho do arquivo para download em MB: "))
+Velocidade_do_link_inserido = float(input("Insira a velocidade do link da internet (Mbps): "))
+Conversão = (Tamanho_do_arquivo_MB_inserido/8)
 
+Resultado_em_Mbts = Conversão / Velocidade_do_link_inserido
 
+Resultado_em_minutos = (Resultado_em_Mbts / 60)
 
+print(f"A velocidade aproximada em minutos é de: {Resultado_em_minutos} minutos")
 
