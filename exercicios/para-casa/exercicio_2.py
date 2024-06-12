0615671641a4f0d490185810c9a806e9a9e52ff9
Danieli Fernandes <danielifernandes33@gmail.com>
2024-06-12 13:03:24 -0300

    altualização do exercicio 2

--- exercicios/para-casa/exercicio_2.py
+++ exercicios/para-casa/exercicio_2.py
@@ -2,15 +2,18 @@
 #calcule e informe o tempo aproximado de download do arquivo usando este link (em minutos). 
 # OBS PARA AGNES RS "SE FOSSE COM METROS/CENTÍMETROS/KM, TERIA APANHADO MENOS KKK"
 
+# https://konnet.com.br/como-calcular-o-tempo-de-download-de-um-arquivo/#:~:text=Tudo%20o%20que%20voc%C3%AA%20precisa,8)%20%3D%20tempo%20em%20segundos.
+#calculo: Tamanho do arquivo em megabytes / (velocidade de download em megabits / 8) =  tempo em segundos.
+#conversão de segundos para minutos = tempo em segundos / 60
 
 
 tamanho_arquivo_mb = float(input("Qual o tamanho para download em MB? "))
 velocidade_internet_mbps = float(input("Qual a velocidade do link de internet em Mbps? "))
 
-velocidade_internet_mbps = velocidade_internet_mbps / 8  # 1 byte = 8 bits
+conversao_mbps_para_mb = velocidade_internet_mbps / 8  # 1 byte = 8 bits, divide o valor da velocidade_internet_mbps por 8 para converter em megabytes
 
-tempo_download_segundos = tamanho_arquivo_mb / velocidade_internet_mbps
-tempo_download_minutos = tempo_download_segundos / 60  # divisão por 60 para converter o tempo para segundos
+tempo_download_segundos = tamanho_arquivo_mb / conversao_mbps_para_mb
+tempo_download_minutos = tempo_download_segundos / 60  # divisão por 60 para converter o tempo para minutos
 
 print(f"O tempo aproximado de download do arquivo é: {tempo_download_minutos:.2f} minutos")
 
