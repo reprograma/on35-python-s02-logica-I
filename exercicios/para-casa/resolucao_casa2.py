a819e4ae48276aef1479a3038fc1b3c65ccbb33e
Thaíse Alves <thaise.lvs11@gmail.com>
2024-06-12 14:05:04 -0300

    Exercícios - Thaíse

--- exercicios/para-casa/resolucao_casa2.py
+++ exercicios/para-casa/resolucao_casa2.py
@@ -3,12 +3,12 @@
 tamanho_arquivo_MB = float(input("Informe o tamanho do aquivo em MB: "))
 velocidade_link_Mbts = float(input("Informe a velocidade do link em Mbps: "))
 
-# 1 MB (Megabyte) tem 8 Mb (Megabit) - converter MB em Mb
-tamanho_arquivo_Mb = tamanho_arquivo_MB / 8
+# 1 MB (Megabyte) tem 8 Mb (Megabits) - converter MB em Mb
+tamanho_arquivo_Mb = tamanho_arquivo_MB * 8
 
-resultado_em_Mbts = tamanho_arquivo_Mb / velocidade_link_Mbts
+tempo_em_segundos = tamanho_arquivo_Mb / velocidade_link_Mbts
 
 # 1 minuto tem 60 segundos - converter segundos em minutos
-tempo_em_minutos = (resultado_em_Mbts / 60)
+tempo_em_minutos = (tempo_em_segundos / 60)
 
 print(f"O tempo aproximado em que o download do arquivo usando este link dura é de {tempo_em_minutos} minutos")
