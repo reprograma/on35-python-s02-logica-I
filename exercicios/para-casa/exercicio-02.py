f62aef48f30d66ab60f4f1bfbfd8161a5ce18dce
Thayná Ingrid <eng.thayingrid@gmail.com>
2024-06-14 00:25:09 -0300

    Entrega Exercicio 01

---
+++ exercicios/para-casa/exercicio-02.py
@@ -0,0 +1,5 @@
+#Exercicio 2 08/06
+
+#Faça um programa que peça o tamanho de um arquivo para download (em MB) e a 
+#velocidade de um link de Internet (em Mbps), 
+#calcule e informe o tempo aproximado de download do arquivo usando este link (em minutos).
